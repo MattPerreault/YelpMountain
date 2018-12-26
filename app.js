a9d0724d25c20731258ef53ee8c8879f717cf2c3
MattPerreault <mattperreault.chew@gmail.com>
2018-12-25 18:01:32 -0700

    updated environment variables.

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
-// mongoose.connect("mongodb://matt:System.in1@ds227373.mlab.com:27373/yelpmountain", {useNewUrlParser: true}); 
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url, {useNewUrlParser: true});
 
 
 app.use(bodyParser.urlencoded({extended: true}));
