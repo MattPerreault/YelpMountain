55f182fa8b94b53b0c1437933b93143c6cfa8b3c
MattPerreault <mattperreault.chew@gmail.com>
2018-12-25 17:28:16 -0700

    Updated db to be on mlab

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
     
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});    
+// mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb://matt:System.in1@ds227373.mlab.com:27373/yelpmountain", {useNewUrlParser: true}); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
